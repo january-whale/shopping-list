e738dd6fe94b57b029807f37534db2f4ce53035a
january-whale <110626769+january-whale@users.noreply.github.com>
2022-08-24 11:03:11 +0300

    Добавила стиль для элементов списка

--- script.js
+++ script.js
@@ -1,4 +1,39 @@
 /* Новые элементы должны добавляться в список по нажатию на Enter */
+const sendInput = document.querySelector('input'); //поле ввода
+const point = document.querySelector('.items'); //куда добавить 
+
+
+sendInput.addEventListener('keydown', function(event) {
+
+
+    if (event.key == 'Enter') {
+
+        const pointText = sendInput.value; //тут хранится то, что клиент ввел в поле ввода
+        const newPoint = document.createElement('div');
+        newPoint.classList.add('items');
+
+        newPoint.textContent = pointText;
+
+        if (pointText != '') {
+            point.append(newPoint); /*для того, чтобы нельзя было отправить пустое сообщение*/
+        }
+
+        sendInput.value = ''; /*для очищения импута после ввода сообщения*/
+
+    }
+
+})
+
+const allPoints = document.querySelectorAll('.items');
+
+for (let allPoint of allPoints) {
+    allPoint.addEventListener('click', function() {
+        allPoint.classList.toggle('done');
+    })
+}
+
+
+
 
 /* Пустые элементы не должны добавляться */
 
